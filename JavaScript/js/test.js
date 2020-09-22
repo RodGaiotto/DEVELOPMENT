b457bc4f0bc199e9eb407d489d897085c08d0d88
Rodrigo <rgaiotto@oc5255651151.ibm.com>
2020-09-22 20:02:54 -0300

    Atualizações jogo helicoptero

--- JavaScript/js/test.js
+++ JavaScript/js/test.js
@@ -1,13 +1,5 @@
-console.log('\nExemplo da utilização de continue');
-
-const array4 = [1, 2, 4, 5, 6];
-
-for (var index2 = 0; index2 < array4.length; index2++) {
-    const element = array4[index2];
-
-    if (element % 2 === 0) {
-        continue;
-    }
-
-    console.log(element);
-}
+let line = split("11 7");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B // Altere o valor da variável com o cálculo esperado
+console.log("X = " + total);
